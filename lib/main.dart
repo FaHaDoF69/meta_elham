4a07c8a9b43fc6d4e37b8e61e18275302c806784
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-10-24 12:03:09 +0300

    Main: dotenv and supabase configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'src/app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables from .env
+  await dotenv.load(fileName: ".env");
+
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL'].toString(),
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'].toString(),
+  );
+
+  // Set up the app
   runApp(App());
 }
